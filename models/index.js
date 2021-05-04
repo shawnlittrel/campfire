a84977d6b9b70f856c717a7f59e5087bced51664
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-03 22:50:58 -0700

    associations done?

--- models/index.js
+++ models/index.js
@@ -3,3 +3,19 @@ const Match = require('./Match');
 const Group = require('./Group');
 
 //create associations
+User.hasMany(Match, {
+     foreignKey: 'user_id'
+});
+
+User.belongsTo(Group, {
+     foreignKey: 'user_id'
+})
+
+User.belongsToMany(Group, {
+     through: Match,
+     foreignKey: 'user_id'
+});
+
+Group.hasMany(Match, {
+     foreignKey: 'group_id'
+});
